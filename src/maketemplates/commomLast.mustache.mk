ee94eb211e7f4a4119d8c4ce5bd6e0a9a93eb97b
cchness <cness@riseup.net>
2021-12-03 07:45:55 -0300

    Atualiza commonLast e remove aspas duplas de pkversion.

--- src/maketemplates/commomLast.mustache.mk
+++ src/maketemplates/commomLast.mustache.mk
@@ -1,38 +1,47 @@
 
-## ############################
-## SELF-GENERATE MAKE (make me)
-##
+## ############################ ##
+## SELF-GENERATE MAKE (make me) ##
+## ############################ ##
 
-mkme_input        = ./make_conf.yaml
-country           = PE
-baseSrc           = /opt/gits/_dg
+mkme_input    = $(shell ls -d "${PWD}/"make_conf.yaml)
+country       = $(shell ls -d "${PWD}" | cut -d'-' -f2 | cut -d'/' -f1)
+baseSrc       = /opt/gits/_dg
 
-srcPy             = $(baseSrc)/preserv/src/run_mustache.py
-mkme_input0       = $(baseSrc)/preserv-$(country)/src/maketemplates/commomFirst.yaml
-mkme_srcTplLast   = $(baseSrc)/preserv-$(country)/src/maketemplates/commomLast.mustache.mk
+mkme_input0   = $(baseSrc)/preserv-$(country)/src/maketemplates/commomFirst.yaml
 
-thisTplFile_root  = $(shell grep 'thisTplFile_root'  < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-sandbox           = $(shell grep 'sandbox'           < $(mkme_input0) | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-schemaId_template = $(shell grep 'schemaId_template' < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
-pkid              = $(shell grep 'pkid'              < $(mkme_input)  | cut -f2 -d':' |  sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_io         = $(shell grep 'pg_io'  < $(mkme_input0) | cut -f2  -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_uri        = $(shell grep 'pg_uri' < $(mkme_input0) | cut -f2- -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pg_db         = $(shell grep 'pg_db'  < $(mkme_input0) | cut -f2  -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
+pkid          = $(shell grep 'pkid'   < $(mkme_input)  | cut -f2  -d':' | sed 's/^[ \t]*//' | sed 's/[\ \#].*//')
 
-mkme_srcTpl       = $(baseSrc)/$(thisTplFile_root)/src/maketemplates/make_$(schemaId_template).mustache.mk
-mkme_output       = $(sandbox)/make_me.mk_$(country)_pkid$(pkid)
+pg_uri_db     = $(pg_uri)/$(pg_db)
 
-readme_srcTpl     = $(baseSrc)/preserv-$(country)/src/maketemplates/readme.mustache
-readme_output     = $(sandbox)/README-draft.md_$(country)_pkid$(pkid)
-
-script_quotes     = $(baseSrc)/preserv/src/quotes.bash
+mkme_output   = $(pg_io)/makeme_$(country)$(pkid)
+readme_output = $(pg_io)/README-draft_$(country)$(pkid)
 
 info:
-	@echo "=== Targets  ==="
+	@echo "=== Targets ==="
+	@printf "me: gera makefile para ingestão dos dados, a partir de make_conf.yaml.\n"
 	@printf "readme: gera rascunho de Readme.md para conjunto de dados.\n"
-	@printf "me: gera makefile para ingestão dos dados, a partir de make_conf.yaml\n"
+	@printf "insert_make_conf.yaml: carrega na base de dados o arquivo make_conf.yaml.\n"
+	@printf "delete_file: deleta arquivo ingestado, a partir do sha256.\n"
 
-readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
-	@echo "-- Create basic readme.md template --"
-	python3 $(srcPy) -b $(baseSrc)/ -t $(readme_srcTpl) -i $(mkme_input)  > $(readme_output)
-	chmod 777 $(readme_output)
+me:
+	@echo "-- Updating this make --"
+	psql $(pg_uri_db) -c "SELECT ingest.lix_generate_makefile('$(country)','$(pkid)');"
+	sudo chmod 777 $(mkme_output)
+	@echo " Check diff, the '<' lines are the new ones... Something changed?"
+	@diff $(mkme_output) ./makefile || :
+	@echo "If some changes, and no error in the changes, move the script:"
+	@echo " mv $(mkme_output) ./makefile"
+	@echo "[ENTER para rodar mv ou ^C para sair]"
+	@read _tudo_bem_
+	mv $(mkme_output) ./makefile
+
+readme:
+	@echo "-- Create basic README-draft.md template --"
+	psql $(pg_uri_db) -c "SELECT ingest.lix_generate_readme('$(baseSrc)','$(country)','$(pkid)');"
+	sudo chmod 777 $(readme_output)
 	@echo " Check diff, the '<' lines are the new ones... Something changed?"
 	@diff $(readme_output) ./README-draft.md || :
 	@echo "If some changes, and no error in the changes, move the readme:"
@@ -41,16 +50,17 @@ readme: $(srcPy) $(mkme_input) $(readme_srcTpl)
 	@read _tudo_bem_
 	mv $(readme_output) ./README-draft.md
 
-me: $(srcPy) $(mkme_input0) $(mkme_input) $(mkme_srcTpl) $(script_quotes)
-	@echo "-- Updating this make --"
-	python3 $(srcPy) -b $(baseSrc)/ -t $(mkme_srcTpl) --tplLast=$(mkme_srcTplLast) -i $(mkme_input) --input0=$(mkme_input0) > $(mkme_output)
-	chmod 777 $(mkme_output)
-	bash $(script_quotes) $(mkme_output)
-	@echo " Check diff, the '<' lines are the new ones... Something changed?"
-	@diff $(mkme_output) ./makefile || :
-	@echo "If some changes, and no error in the changes, move the script:"
-	@echo " mv ./makefile ./bk-makefile"
-	@echo " mv $(mkme_output) ./makefile"
-	@echo "[ENTER para rodar mv ou ^C para sair]"
+insert_make_conf.yaml:
+	@echo "-- Carrega make_conf.yaml na base de dados. --"
+	@echo "Uso: make insert_make_conf.yaml"
+	@echo "pkid: $(pkid)"
+	@echo "[ENTER para continuar ou ^C para sair]"
 	@read _tudo_bem_
-	mv $(mkme_output) ./makefile
+	psql $(pg_uri_db) -c "SELECT ingest.lix_insert('$(country)','$(mkme_input)','make_conf');"
+
+delete_file:
+	@echo "Uso: make delete_file hash=<inicio do hash do arquivo>"
+	@echo "hash: $(hash)"
+	@echo "[ENTER para continuar ou ^C para sair]"
+	@read _tudo_bem_
+	@[ "${hash}" ] && psql $(pg_uri_db) -c "DELETE FROM ingest.layer_file WHERE pck_fileref_sha256 LIKE '$(hash)%'" || ( echo "hash não informado.")
